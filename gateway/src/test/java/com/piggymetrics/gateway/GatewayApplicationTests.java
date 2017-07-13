810ffe74b58d872da0a0dc94a24ff4d214459ed2
luochunsong <luochunsong@e.hunantv.com>
2017-07-13 17:05:18 +0800

    add change

--- gateway/src/test/java/com/piggymetrics/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/piggymetrics/gateway/GatewayApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.MOCK)
 @WebAppConfiguration
 public class GatewayApplicationTests {
 
